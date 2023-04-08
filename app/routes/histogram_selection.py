4cf961984d15b8972fb16871f292cd0a03c6c452
gabriele <gabriele.sau2598@gmail.com>
2023-04-08 19:28:20 +0200

    Implementation of histogram display in the GUI.

--- app/routes/histogram_selection.py
+++ app/routes/histogram_selection.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 from app.forms.histogram_form import HistogramForm
+from ml.classification_utils import create_histogram
 
 
 @app.route('/histograms', methods=['GET', 'POST'])
@@ -10,5 +11,10 @@ def histograms():
     the histogram."""
     form = HistogramForm()
     if form.validate_on_submit():
-        pass
+        image_id = form.image.data
+
+        hist_path = 'app/static/Histogram'
+        hist_id = create_histogram(image_id, hist_path)
+
+        return render_template('histogram_output.html', image_id=image_id, histogram=hist_id)
     return render_template('histogram_selection.html', form=form)
