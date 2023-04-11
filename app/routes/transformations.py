aa7d30eaff5ec421055e2add260aef84840e3907
gabriele <gabriele.sau2598@gmail.com>
2023-04-11 19:57:48 +0200

    API that applies the transformation implemented.

--- app/routes/transformations.py
+++ app/routes/transformations.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 from app.forms.transformation_form import TransformationForm
+from ml.classification_utils import image_transformation
 
 
 @app.route('/transformation', methods=['GET', 'POST'])
@@ -10,5 +11,13 @@ def transformations():
     the result of the transformation."""
     form = TransformationForm()
     if form.validate_on_submit():
-        pass
+        image_id = form.image.data
+        color_en = form.color.data
+        contrast_en = form.contrast.data
+        brightness_en = form.brightness.data
+        sharpness_en = form.sharpness.data
+
+        transformation_path = 'app/static/Transformations'
+        transformation_id = image_transformation(image_id, transformation_path, color_en, contrast_en,
+                                                 brightness_en, sharpness_en)
     return render_template('transformation_select.html', form=form)
